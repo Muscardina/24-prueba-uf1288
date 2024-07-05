e3ee1ed82c40ec69721d6067e77eb48ccd34bf83
Muscardina <vergonzalezsainz@gmail.com>
2024-07-05 10:17:47 +0200

    Comprobar paridad matriz

--- main.js
+++ main.js
@@ -1,12 +1,36 @@
 //Comprobar si la paridad de la siguiente matriz es la correcta:
 
-let bytes-a-comprobar = [
-[1, 0, 0, 1, 1, 1, 0, 1, 1],
-[0, 1, 1, 0, 0, 1, 0, 1, 0],
-[0, 0, 1, 1, 1, 0, 1, 1, 1],
-[1, 1, 0, 1, 0, 1, 0, 0, 0],
-[1, 1, 0, 0, 1, 0, 1, 0, 0],
-[0, 1, 1, 1, 0, 0, 1, 0, 0],
-[1, 0, 1, 0, 0, 1, 1, 1, 1],
-[0, 0, 1, 1, 0, 0, 0, 0, 1]
-]
+let matriz = [
+  [1, 0, 0, 1, 1, 1, 0, 1, 1],
+  [0, 1, 1, 0, 0, 1, 0, 1, 0],
+  [0, 0, 1, 1, 1, 0, 1, 1, 1],
+  [1, 1, 0, 1, 0, 1, 0, 0, 0],
+  [1, 1, 0, 0, 1, 0, 1, 0, 0],
+  [0, 1, 1, 1, 0, 0, 1, 0, 0],
+  [1, 0, 1, 0, 0, 1, 1, 1, 1],
+  [0, 0, 1, 1, 0, 0, 0, 0, 1],
+];
+//Se suman los 1 de la matriz para ver si devuelven un número par o impar
+function sumMatriz(matriz) {
+  let sum = 0;
+  for (let i = 0; i < matriz.length; i++) {
+    for (let j = 0; j < matriz[i].length; j++) {
+      sum += matriz[i][j];
+    }
+  }
+  return sum;
+}
+
+console.log(sumMatriz(matriz));
+//Comprueba la paridad del número devuelto e indica si la matriz tiene algun fallo de paridad
+let sumatorio = sumMatriz(matriz);
+
+function esPar() {
+  if (sumatorio % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(esPar(sumatorio));
